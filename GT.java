b91e8b6cd33f9e15a5b6212698001e01d1dc99a3
Nofear <off449421@gmail.com>
2025-11-20 10:36:29 +0545

    new file

---
+++ GT.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class GT here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GT
+{
+    public static void main(String[] args){
+        
+        System.out.println("Learing git with BlueJ");
+    }
+}
